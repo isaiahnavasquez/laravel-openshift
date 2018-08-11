c6dcfead974901e1b1d0362398830cb199a45c96
isaiahnavasquez <isaiahnavasquez@gmail.com>
2018-08-11 20:52:48 +0800

    Update database.php

--- config/database.php
+++ config/database.php
@@ -41,7 +41,7 @@ return [
 
         'mysql' => [
             'driver' => 'mysql',
-            'host' => env('DB_HOST', 'mysql'),
+            'host' => env('DB_HOST', 'localhost'),
             'port' => env('DB_PORT', '3306'),
             'database' => env('DB_DATABASE', 'kristine'),
             'username' => env('DB_USERNAME', 'laravel'),
