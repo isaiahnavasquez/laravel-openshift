ba2a95f930a9ec14a5d4155b3f18badb8375cd67
isaiahnavasquez <isaiahnavasquez@gmail.com>
2018-08-11 20:00:22 +0800

    Update database.php

--- config/database.php
+++ config/database.php
@@ -45,7 +45,7 @@ return [
             'port' => env('DB_PORT', '3306'),
             'database' => env('DB_DATABASE', 'kristine'),
             'username' => env('DB_USERNAME', 'laravel'),
-            'password' => env('DB_PASSWORD', 'jFogwMbXDrvxYUtQ'),
+            'password' => env('DB_PASSWORD', 'laravel'),
             'unix_socket' => env('DB_SOCKET', ''),
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
